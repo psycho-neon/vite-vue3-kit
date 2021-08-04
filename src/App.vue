cb5c69e7d722e8af5ca8b96ac319513a3be474d2
Psycho.Neon <git@xkromui.com>
2021-08-04 08:09:54 +0300

    Updated to Vue3 Composition API

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,18 @@
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+export default {
+  components: {
+    HelloWorld,
+  }
+  
+  setup() {
+  
+  }
+}
 </script>
 
 <style>
